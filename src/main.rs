496784719b37fe4448b022cc2ecd1e6de0a9603c
john <jhopkin@protonmail.com>
2023-04-21 21:10:45 +1000

    updated

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use std::fs::OpenOptions;
 use hyper::{client::HttpConnector, Body, Client, StatusCode};
 use std::time::*;
-use std::env;
-use std::env::args;
+// use std::env::args;
 
 type ReqClient = Client<HttpConnector, Body>;
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct RequestMetrics {
     task: Option<u64>,
     number_of_requests: u64,
@@ -32,43 +30,49 @@ impl RequestMetrics {
     }
 }
 
-// #[derive(Debug)]
-// struct RequestOptions {
-//     url: Option<String>,
-//     client: Option<ReqClient>,
-//     iterations: Option<u64>,
-//     task: Option<u64>,
-//     request_info: Option<RequestMetrics>
-//
-// }
-// impl RequestOptions {
-//     fn new() -> Self {
-//         RequestOptions{
-//             url:None,
-//             client: None,
-//             iterations: None,
-//             task: None,
-//             request_info: None
-//         }
-//     }
-// }
+#[derive(Debug, Clone)]
+struct RequestOptions {
+    url: String,
+    iterations: u64,
+    task: u64,
+    request_info: RequestMetrics
+
+}
+impl RequestOptions {
+    fn new() -> Self {
+        RequestOptions{
+            url:"".to_string(),
+            iterations: 0,
+            task: 0,
+            request_info: RequestMetrics::new()
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
 
-    let iterations = 10000;
+    let iterations = 1000;
     let tasks = 12;
     let mut count = 0;
+    let mut options = RequestOptions::new();
+
     let mut connections: Vec<ReqClient> = Vec::new();
     let mut handles = Vec::new();
     let mut durations: Vec<Duration> = vec![];
     let start = Instant::now();
+    let mut metrics = RequestMetrics::new();
+    options.iterations = 1000;
+    options.task = tasks;
+
     for _ in 0..tasks {
         connections.push(ReqClient::new());
     }
     for client in connections {
+        options.task = count;
+        let mut options_clone  = options.clone();
         let handle = tokio::spawn(async move {
-            send_request(client, iterations, count).await;
+            send_request(client, &mut options_clone).await;
         });
         handles.push(handle);
         count = count + 1;
@@ -82,15 +86,17 @@ async fn main() {
     calculate_avg_response_time(durations.clone(), iterations);
 }
 
-async fn send_request(client: ReqClient, iterations: u64, task: u64) -> RequestMetrics{
+async fn send_request(client: ReqClient, options: &mut RequestOptions) -> Vec<RequestMetrics>{
+    let mut vec_of_metrics = Vec::new();
+    let mut metrics = RequestMetrics::new();
     let task_start = Instant::now();
     let mut count = 0;
-    let mut request_info = RequestMetrics::new();
+    let request_info = RequestMetrics::new();
+    let url = hyper::Uri::from_static("http://localhost:8088/dinosaurs/era/cretaceous");
+    while count < options.iterations {
 
-    while count < iterations {
-        let url = hyper::Uri::from_static("http://localhost:8088/dinosaurs/era/cretaceous");
         // http://localhost:8088/dinosaurs/era/cretaceous
-        let resp = client.get(url).await;
+        let resp = client.get(url.clone()).await;
         // we can verify that there are concurrent requests going out by merely logging the
         // task, along side what iteration of request we are sending. This way we can see the
         // requests are interleaved and not being sent sequentially one task followed by another.
@@ -102,18 +108,18 @@ async fn send_request(client: ReqClient, iterations: u64, task: u64) -> RequestM
         // );
         count = count + 1;
         if resp.unwrap().status() == StatusCode::OK {
-            request_info.successful_requests = request_info.successful_requests + 1;
+            metrics.successful_requests = metrics.successful_requests + 1;
         }
-        request_info.task = Some(task);
-        request_info.number_of_requests = count;
-
-
+        metrics.task = Some(options.task);
+        metrics.number_of_requests = count;
     }
     let task_finish = task_start.elapsed();
-    request_info.connection_duration = task_finish;
-    request_info.rate_of_requests_by_connection();
-    println!("{:?}", request_info);
-    request_info
+    metrics.connection_duration = task_finish;
+    metrics.rate_of_requests_by_connection();
+
+    vec_of_metrics.push(metrics);
+    println!("{:?}", vec_of_metrics);
+    vec_of_metrics
 }
 // wrc appears to be more than ten times faster, presumably because it is using multiple threads?
 // In production it the results from the current solution may not be realistic simulation of
